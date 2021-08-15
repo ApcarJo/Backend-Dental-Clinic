1169d96532d75e5ea29a4ddd1dc42141e7223006
José Luis <hansokaz@gmail.com>
2021-08-15 19:05:26 +0200

    changed port index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const router = require('./router');
-const port = 3006;
+const port = process.env.PORT || 3006;
 const db = require("./config/mongoose");
 const cors = require('cors');
 
